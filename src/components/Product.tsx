7014abcc9e42932f4e0e5a9cf3029aa909103df5
Patrice Bille <pbille@inno-it.es>
2024-04-13 23:41:53 +0200

    add product and row

---
+++ src/components/Product.tsx
@@ -0,0 +1,91 @@
+import { useDrag, useDrop } from "react-dnd";
+import { Identifier } from "dnd-core/src/interfaces";
+import { Box, Typography } from "@mui/material";
+import { Product as ProductType } from "@/common/types";
+import { ItemTypes } from "@/common/Constants";
+import { useRef } from "react";
+import useDragMoves from "@/hooks/useDragMoves";
+
+type Props = {
+  product: ProductType;
+  index: number;
+  rowIndex: number;
+  rowIdentifier: Identifier | null;
+};
+
+type DragItem = ProductType & {
+  index: number;
+  row: number;
+};
+
+const Product = ({ product, index, rowIdentifier, rowIndex }: Props) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const {handleProductOrderMove} = useDragMoves();
+
+  const [{ opacity }, drag] = useDrag(
+    () => ({
+      type: ItemTypes.Product,
+      item: { ...product, index, row: rowIndex, rowIdentifier },
+      collect: (monitor) => ({
+        opacity: monitor.isDragging() ? 0.5 : 1,
+      }),
+    }),
+    [rowIdentifier, rowIndex],
+  );
+
+  const [, drop] = useDrop<DragItem>({
+    accept: ItemTypes.Product,
+    hover(item) {
+      if (!ref.current) {
+        return;
+      }
+
+      const dragIndex = item.index;
+      const hoverIndex = index;
+      const dragRow = item.row;
+      const hoverRow = rowIndex;
+
+      if (dragIndex === hoverIndex && dragRow === hoverRow) {
+        return;
+      }
+
+      handleProductOrderMove(dragIndex, hoverIndex, dragRow, hoverRow);
+      item.index = hoverIndex;
+      item.row = hoverRow;
+    },
+  });
+
+  drag(drop(ref));
+
+  return (
+    <>
+      {!!product?.id && (
+        <Box
+          ref={ref}
+          sx={{
+            opacity,
+            width: 150,
+            borderWidth: 1,
+            borderStyle: "solid",
+            borderColor: "#b9b9b9",
+            padding: 2,
+            borderRadius: 2,
+          }}
+        >
+          <Typography variant="caption" style={{ wordWrap: "break-word" }}>
+            {product.name}
+          </Typography>
+          <img
+            src={product.image}
+            alt={product.name}
+            width={"100%"}
+            loading="lazy"
+          />
+          <Typography variant="caption">{product.price}</Typography>
+        </Box>
+      )}
+    </>
+  );
+};
+
+export default Product;
---
+++ src/components/Row.tsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  Stack,
+  Typography,
+} from "@mui/material";
+import { GridRow } from "@/common/types";
+import { TemplateDirection } from "@/common/Constants";
+import DroppableArea from "@/components/DroppableArea";
+import useRowDrag from "@/hooks/useRowDrag";
+import SelectProductButton from "@/components/SelectProductButton";
+import DeleteRowButton from "@/components/DeleteRowButton";
+
+type Props = {
+  row: GridRow;
+  index: number;
+};
+
+const Row = ({ row, index }: Props) => {
+  const { ref, handlerId } = useRowDrag({ row, index });
+  const [rowPosition, setRowPosition] = useState(row.position);
+
+  const handleSelectTemplate = (event: SelectChangeEvent) => {
+    setRowPosition(event.target.value);
+  };
+
+  return (
+    <Stack
+      gap={1}
+      padding={2}
+      ref={ref}
+      data-handler-id={handlerId}
+      sx={{
+        backgroundColor: "rgba(236,236,236,0.22)",
+        border: "1px solid #d9d9d9",
+        borderRadius: 1,
+      }}
+    >
+      <Stack direction="row" alignItems="center" justifyContent="space-between">
+        <Typography color="dimgray">{row.name}</Typography>
+        <Stack direction="row" alignItems="center" gap={1}>
+          <SelectProductButton rowId={row.id} />
+          <FormControl size="small">
+            <InputLabel id={`row-position-${row.id}`}>Position</InputLabel>
+            <Select
+              labelId={`row-position-${row.id}`}
+              id={`select-position-${row.id}`}
+              value={rowPosition}
+              label="Position"
+              onChange={handleSelectTemplate}
+            >
+              <MenuItem value={TemplateDirection.RIGHT}>Izquierda</MenuItem>
+              <MenuItem value={TemplateDirection.CENTER}>Centro</MenuItem>
+              <MenuItem value={TemplateDirection.LEFT}>Derecha</MenuItem>
+            </Select>
+          </FormControl>
+        </Stack>
+      </Stack>
+      <DroppableArea
+        key={row.id}
+        row={row}
+        rowTemplate={rowPosition}
+        rowIndex={index}
+      />
+      <Stack direction="row" justifyContent="flex-end">
+        <DeleteRowButton rowId={row.id} />
+      </Stack>
+    </Stack>
+  );
+};
+
+export default Row;
